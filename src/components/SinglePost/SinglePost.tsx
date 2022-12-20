d4489d16cabc811b9bedfb0f65253744ba366f9a
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-12-20 22:08:42 +0900

    pageFixed

--- src/components/SinglePost/SinglePost.tsx
+++ src/components/SinglePost/SinglePost.tsx
@@ -1,17 +1,46 @@
+import { Card } from 'antd';
 import { Content } from 'antd/es/layout/layout';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Thread } from '../../interfaces';
 
 type SinglePostProps = {
 
 };
-
 const SinglePost:React.FC<SinglePostProps> = () => {
-    const { id } = useParams()
+
+const { id } = useParams()
+const [loading, setLoading] = useState(false);
+const [data, setData] = useState<Thread>();
+
+const loadPost = () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    fetch(`http://localhost:8000/thread/thread/${id}`)
+      .then((res) => res.json())
+      .then((body) => {
+        setData(body.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+    useEffect(() => {
+        loadPost();
+    }, []);
+
+
+  
     return (
     <Content style={{ margin: '24px 16px 0' }}>
-        <div>
-            {id} is the mongoID
+        <div className="site-card-border-less-wrapper">
+            <Card title={data?.title} bordered={false} >
+                <p>{data?.content}</p>
+            </Card>
         </div>
     </Content>);
 }
