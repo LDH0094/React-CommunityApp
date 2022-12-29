7e143348406f67846d89c2f04f9979526f537984
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-12-29 21:05:21 +0900

    layout fixed

--- src/components/NavigationMenu/BasicNavigationMenu.tsx
+++ src/components/NavigationMenu/BasicNavigationMenu.tsx
@@ -1,41 +1,63 @@
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Typography, Space, MenuProps } from 'antd';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { menuState } from '../../atoms/menuAtom';
 
 type BasicNavigationMenuProps = {
     
 };
 
 const BasicNavigationMenu:React.FC<BasicNavigationMenuProps> = () => {
-    // const [selectItem, setSelecItem] = useState("게시판 목록 ⭐️");
+  const [selectedKey, setSelectedKey] = useRecoilState(menuState);
+  const navigate = useNavigate();
 
   const items: MenuProps["items"] = [
     {
-      key: 1,
+      key: '1',
       label: "자유 게시판",
       onClick: () => {
-        // setSelecItem("자유 게시판");
+        setSelectedKey({itemSelected: '1'})
+        navigate('/')
       },
     },
     {
-      key: 2,
+      key: '2',
       label: "QT 나눔",
       onClick: () => {
-        // setSelecItem("QT 나눔");
+        setSelectedKey({itemSelected: '2'})
+        navigate('/QT')
       },
     },
+    {
+        key: '3',
+        label: "공지",
+        onClick: () => {
+          setSelectedKey({itemSelected: '3'})
+          navigate('/notifications')
+        },
+      },
+      {
+        key: '4',
+        label: "로그인 / 회원가입",
+        onClick: () => {
+          setSelectedKey({itemSelected: '4'})
+          navigate('/users')
+        },
+      },
   ];
     
     return <Dropdown
     menu={{
       items,
       selectable: true,
-      defaultSelectedKeys: ['1']
+      defaultSelectedKeys: [selectedKey.itemSelected],
     }}
   >
     <Typography.Link>
       <Space>
-        {"게시판 목록 ⭐️"}
+        {"게시판 목록 ⛪️"}
         <DownOutlined />
       </Space>
     </Typography.Link>
